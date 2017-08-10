2933154c1782fbcec673e33d0da191a8be795d4d
duanguiyuan <duanguiyuan@gbcom.com.cn>
2017-08-10 16:05:26 +0800

    2.结构体net_device_ops 新增成员ndo_add_vxlan_port ndo_del_vxlan_port

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1001,6 +1001,12 @@ struct net_device_ops {
 						    netdev_features_t features);
 	int			(*ndo_neigh_construct)(struct neighbour *n);
 	void			(*ndo_neigh_destroy)(struct neighbour *n);
+	void			(*ndo_add_vxlan_port)(struct  net_device *dev,
+						      sa_family_t sa_family,
+						      __be16 port);
+	void			(*ndo_del_vxlan_port)(struct  net_device *dev,
+						      sa_family_t sa_family,
+						      __be16 port);
 };
 
 /*
