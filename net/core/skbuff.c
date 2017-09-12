88a71fd16fbe21d1c2db976434f86555d3d62ca1
duanguiyuan <duanguiyuan@gbcom.com.cn>
2017-09-12 17:29:30 +0800

    2.补充 ¼

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -3366,7 +3366,7 @@ void skb_scrub_packet(struct sk_buff *skb, bool xnet)
 	skb->tstamp.tv64 = 0;
 	skb->pkt_type = PACKET_HOST;
 	skb->skb_iif = 0;
-	//skb->ignore_df = 0;
+	skb->ignore_df = 0;
 	skb_dst_drop(skb);
 	secpath_reset(skb);
 	nf_reset(skb);
