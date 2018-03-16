b3546b271526da09c0230dc297289606a971a8d4
duanguiyuan <duanguiyuan@gbcom.com.cn>
2018-03-16 18:21:24 +0800

    删除多余文件 上传.wqconfig文件

--- include/generated/compile.h
+++
@@ -1,6 +0,0 @@
-/* This file is auto generated, version 2 */
-#define UTS_MACHINE "mips"
-#define UTS_VERSION "#2 Wed Jul 19 17:34:39 CST 2017"
-#define LINUX_COMPILE_BY "duanguiyuan"
-#define LINUX_COMPILE_HOST "build.gbcom.com.cn"
-#define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (Linaro GCC 4.6-2012.02) "
--- usr/.initramfs_data.cpio.d
+++
@@ -1,1 +0,0 @@
-deps_initramfs := /home/duanguiyuan/SpiderNet/TA361/build_dir/linux-ar71xx_generic/linux-3.3.8/scripts/gen_initramfs_list.sh \
